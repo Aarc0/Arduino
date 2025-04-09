5e7912b9d69aa71199f9a7307845d0b0ef67a1b3
Aarc0 <andres111alejandro@hotmail.com>
2025-04-09 19:56:50 +0200

    Robot listo para pruebas

--- DESPERTADOR/src/Despertador.cpp
+++ DESPERTADOR/src/Despertador.cpp
@@ -11,19 +11,20 @@ volatile bool alarma = false;
 void Desactivar_Alarma()
 {
     alarma = !alarma;
+    //Esta función vuelve la variable "alarma" en falso si es verdadera, es decir, desactiva la alarma
 }
 
 void setup() 
 {
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(Buttonpin),Desactivar_Alarma,RISING);
+  attachInterrupt(digitalPinToInterrupt(Buttonpin),Desactivar_Alarma,RISING); //Si presiono al boton al momento de dejar de presionarlo llamo a la función del medio
   pinMode(led_rojo,OUTPUT);
   pinMode(led_verde,OUTPUT);
 }
 
 void loop() 
 {
-    interrupts();
+    interrupts(); //Con esto activo las interrupciones 
     int Nivel_luz = analogRead(A0);
     if(Nivel_luz>400) alarma = !alarma;
     
@@ -37,6 +38,7 @@ void loop()
         digitalWrite(led_rojo,LOW);
         digitalWrite(led_verde,HIGH);
         delay(100);
+        //Si quiero desactivar la alarma tengo que presionar el boton (Linea 19)
     }   
     else 
     {
