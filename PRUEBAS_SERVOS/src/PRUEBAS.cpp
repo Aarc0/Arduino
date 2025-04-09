1b882e8ba7110bd6a3bbf23162af8b651c4ec9ca
Aarc0 <andres111alejandro@hotmail.com>
2025-04-09 20:18:42 +0200

    pruebas servos

---
+++ PRUEBAS_SERVOS/src/PRUEBAS.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#define SERVOSTOP 380
+
+#define servo_left 0
+#define servo_right 1
+
+void setup() 
+{
+  pwm.begin();
+  pwm.setPWMFreq(60);
+}
+
+void loop() 
+{
+  pwm.setPWM(servo_left,0,SERVOSTOP);
+  pwm.setPWM(servo_right,0,SERVOSTOP);
+}
+
+
