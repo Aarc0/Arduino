a8555aa858188ba841a9d39cb20ea248420138a5
Aarc0 <andres111alejandro@hotmail.com>
2025-04-10 14:30:19 +0200

    Seguir con robot

---
+++ PRUEBAS_SERVOS/src/main.cpp
@@ -0,0 +1,116 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+//MOVIMIENTO DE LAS RUEDAS
+#define SERVOSTOP 380
+#define SERVODERECHA 390
+#define SERVOIZQUIERDA 370
+
+//RUEDAS
+#define servo_left 0
+#define servo_right 1
+
+//INFRAROJOS
+#define IR_left 2
+#define IR_right 3
+
+//GIRO POR ANGULOS
+#define SERVO_90GRADOS 420
+int servo_180 = 2; //El servo está conectado al canal 2
+
+  //////////////////////////////////////////////////
+ ////        DECLARACIÓN DE FUNCIONES          ////
+//////////////////////////////////////////////////
+
+
+
+  //////////////////////////////////////////////////
+ ////        FIN DECLARACIÓN FUNCIONES         ////
+//////////////////////////////////////////////////
+
+void setup() 
+{
+  pinMode(IR_left,INPUT);
+  pinMode(IR_right,INPUT);
+  pwm.begin();
+  pwm.setPWMFreq(60);
+  Serial.begin(9600);
+
+  pwm.setPWM(servo_left,0,SERVOSTOP);
+  pwm.setPWM(servo_right,0,SERVOSTOP);
+
+}
+
+void loop() 
+{
+  int IR_Izquierdo = digitalRead(IR_left);
+  int IR_Derecho = digitalRead(IR_right);
+
+  /*
+  Blanco = 1
+  Negro = 0
+  */
+  //Con este bloque imprimo una linea entera con el valor de los IR en dicho momento
+  /////////////////////////////////////////////
+  Serial.print("IR_Izquierdo: ");
+  Serial.print(digitalRead(IR_left));
+  Serial.print("      IR_Derecho: ");
+  Serial.println(digitalRead(IR_right));
+  /////////////////////////////////////////////
+
+  /*
+    Con esta parte de codigo puede ir hacia atras
+    pwm.setPWM(servo_left,0,SERVODERECHA);
+    pwm.setPWM(servo_right,0,SERVOIZQUIERDA);
+  */
+  
+  /*
+    Con esta parte de código hago que vaya hacia adelante
+    pwm.setPWM(servo_left,0,SERVOIZQUIERDA);
+    pwm.setPWM(servo_right,0,SERVODERECHA);
+  */
+  
+
+  /*
+    Con esta parte hago que gire hacia la derecha
+    pwm.setPWM(servo_left,0,SERVOIZQUIERDA);
+    pwm.setPWM(servo_right,0,SERVOSTOP);
+  */
+  
+  /*
+    Con esta parte hago que gire hacia la izquierda
+    pwm.setPWM(servo_left,0,SERVOSTOP);
+    pwm.setPWM(servo_right,0,SERVODERECHA);
+  */
+
+  //Con esto hago que siempre vaya hacia adelante
+  pwm.setPWM(servo_left,0,SERVOIZQUIERDA);
+  pwm.setPWM(servo_right,0,SERVODERECHA);
+  
+  if(IR_Izquierdo == 1 && IR_Derecho == 0)
+  {
+    //Con esto le digo que si el izquierdo ve blanco se vaya un poco a la derecha
+    pwm.setPWM(servo_left,0,360);
+    pwm.setPWM(servo_right,0,SERVOSTOP);
+  }
+  else if(IR_Izquierdo == 0 && IR_Derecho == 1)
+  {
+    //Con esto le digo que si el izquierdo ve blanco se vaya un poco a la derecha
+    pwm.setPWM(servo_left,0,SERVOSTOP);
+    pwm.setPWM(servo_right,0,400);
+  }
+
+  
+ /*
+  else if(IR_Izquierdo == 1 && IR_Derecho == 1)
+  {
+    pwm.setPWM(servo_left,0,SERVOSTOP);
+    pwm.setPWM(servo_right,0,SERVOSTOP);
+  }
+ 
+ */ 
+}
+
